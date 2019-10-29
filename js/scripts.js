529707022b97d9b6a2820f8400e52b79c6dc4254
Paul <paul.hesling@gmail.com>
2019-10-29 15:04:56 +0100

    quick fix 3.1

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// Selects the FIRST occurance of <button>;
+// Veranderd de css bij het klikken op de zoekknop
 var button = document.querySelector("header form button");
 var element = document.querySelector("header form input");
 
